925dbb25e05c352a2de9088cc488e89725a5a70e
Ryuji-Kishima-Fella <ryujikijima911@gmail.com>
2025-11-05 22:49:35 +0700

    New Feature: Mood Summary
    feat: add mood summary feature
    
    Added a new feature to display a summary of mood statistics and show the most frequent mood.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # mood_tracker
+
 A simple Python app to record and track my daily moods.
+
+
+## Features
+- Log how you feel each day
+- View your modd history
+- Saves data locally in 'mood_log.txt'
+
+## Future Improvements
+- Add charts/graphs
+- Add daily reminders
+- Turn into a web or mobile app
+
+## How to Run
+1. Install Python 3
+2. Run 'py mood_tracker.py' in your terminal. 
--- mood_tracker.py
+++ mood_tracker.py
@@ -17,11 +17,36 @@ def show_moods():
     except FileNotFoundError:
         print("No mood records found yet. Start by logging your first mood!")
 
+def show_summary():
+    try:
+        with open("mood_log.txt", "r") as file:
+            moods = [line.split("-")[1].strip().lower() for line in file]
+            total = len(moods)
+
+            if total == 0:
+                print("No mood records found yet.")
+                return
+
+            unique_moods = {}
+            for mood in moods:
+                unique_moods[mood] = unique_moods.get(mood, 0) + 1
+            
+            print("\n📊 Mood Summary:")
+            for mood, count in unique_moods.items():
+                print(f"    {mood.capitalize()}: {count} times")
+
+            most_common = max(unique_moods, key=unique_moods.get)
+            print(f"\nMost frequent mood: 😌 {most_common.capitalize()}")
+    except FileNotFoundError:
+        print("No mood records found yet. Start by logging your first mood!")
+
+  
 def main():
     print("=== Mood Tracker ===")
     print("1. Log today's mood")
     print("2. View mood history")
-    print("3. Exit")
+    print("3. View mood summary")
+    print("4. Exit")
 
     choice = input("Enter your choice: ")
 
@@ -29,6 +54,8 @@ def main():
         log_mood()
     elif choice == "2":
         show_moods()
+    elif choice == "3":
+        show_summary()
     else:
         print("Goodbye!")
 
